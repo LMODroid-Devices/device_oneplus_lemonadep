9fc4a57e76ca88f468661a5838c5112d7b9e024b
LMO GerritBot <gerrit@libremobileos.com>
2023-04-21 22:25:47 +0000

    Adapt to LMODroid by device-add script
    Change-Id: Ic9c2b986cc2f23a9b3e9e91a1d91db920583134b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonadep.mk
+    $(LOCAL_DIR)/lmodroid_lemonadep.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonadep-user \
-    lineage_lemonadep-userdebug \
-    lineage_lemonadep-eng
+    lmodroid_lemonadep-user \
+    lmodroid_lemonadep-userdebug \
+    lmodroid_lemonadep-eng
